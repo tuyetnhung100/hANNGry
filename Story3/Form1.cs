705a392dac2b3508cd15981e4d154323e4e3dd94
nic-zern <nic.zern@pcc.edu>
2019-10-16 18:21:46 -0700

    Added comments to the code

--- Story3/Form1.cs
+++ Story3/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+// This form is for creating templates for the notification sending in Story 2.
+// Author: Nic Zern
 namespace Story3
 {
     public partial class templateCreator : Form
@@ -22,16 +25,19 @@ namespace Story3
 
         }
 
+        // Inserts the tag for the student name.
         private void studentNameTagButton_Click(object sender, EventArgs e)
         {
             templateRichTextBox.SelectedText = "{$student.name}";
         }
 
+        // Inserts the tag for the staff name.
         private void staffNameTagButton_Click(object sender, EventArgs e)
         {
             templateRichTextBox.SelectedText = "{$staff.name}";
         }
 
+        // Clears the rich text box.
         private void clearAllButton_Click(object sender, EventArgs e)
         {
             DialogResult clear = MessageBox.Show("You are about to clear the template text box. Are you sure?", "Warning!", MessageBoxButtons.YesNo);
