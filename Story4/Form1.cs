538fe677d7fc70228e25bf53af5d84c587c1fd8f
Alex Matthias <alex.matthias@pcc.edu>
2019-10-31 17:16:41 -0700

    Added if else statement to make it so first date must be before second.

--- Story4/Form1.cs
+++ Story4/Form1.cs
@@ -51,43 +51,46 @@ namespace Story4
             
             DateTime endTime = TimeReset.ResetTimeToEndOfDay(secondDateTimePicker.Value);
 
-            List<Notification> notifications = new List<Notification>();
-            NotificationDB.Load(startTime, endTime, ref notifications);
-            DataTable datatable = new DataTable();
-            datatable.Columns.Add("Sender");
-            datatable.Columns.Add("Subject");
-            datatable.Columns.Add("Message");
-            datatable.Columns.Add("Date Sent");
-            datatable.Columns.Add("Number Sent");
-
-            foreach (Notification notification in notifications)
+            if (endTime < startTime)
             {
-                datatable.Rows.Add(
-                    notification.SenderName,
-                    notification.Subject,
-                    notification.Message,
-                    notification.SentDate.ToString("MM/dd/yyyy HH:mm"),
-                    notification.NumberSent
-                );
+                MessageBox.Show("The first date must be before the second date!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                List<Notification> notifications = new List<Notification>();
+                NotificationDB.Load(startTime, endTime, ref notifications);
+                DataTable datatable = new DataTable();
+                datatable.Columns.Add("Sender");
+                datatable.Columns.Add("Subject");
+                datatable.Columns.Add("Message");
+                datatable.Columns.Add("Date Sent");
+                datatable.Columns.Add("Number Sent");
+
+                foreach (Notification notification in notifications)
+                {
+                    datatable.Rows.Add(
+                        notification.SenderName,
+                        notification.Subject,
+                        notification.Message,
+                        notification.SentDate.ToString("MM/dd/yyyy HH:mm"),
+                        notification.NumberSent
+                    );
+                }
+
+                notificationDataGridView.DataSource = datatable;
+
+                notificationDataGridView.Columns[0].Width = 75;
+                notificationDataGridView.Columns[1].Width = 85;
+                notificationDataGridView.Columns[2].Width = 585;
+                notificationDataGridView.Columns[3].Width = 100;
+                notificationDataGridView.Columns[4].Width = 50;
+
             }
 
-            notificationDataGridView.DataSource = datatable;
-
-            notificationDataGridView.Columns[0].Width = 75;
-            notificationDataGridView.Columns[1].Width = 85;
-            notificationDataGridView.Columns[2].Width = 585;
-            notificationDataGridView.Columns[3].Width = 100;
-            notificationDataGridView.Columns[4].Width = 50;
 
 
 
 
-
-            //foreach (DataGridViewRow r in notificationDataGridView.Rows)
-            //{
-            //    r.Height = 100;
-            //}
-            // notificationDataGridView.Rows[0].Height = 50;
 
 
 
