90efbce420bea2fcec0645286f5dcdfed83dbcd9
Gong-Hao Wei <gonghao.wei@pcc.edu>
2019-11-05 18:23:17 -0800

    fix MainForm

--- Management/MainForm.cs
+++ Management/MainForm.cs
@@ -8,6 +8,20 @@ namespace Management
         public MainForm(Account account)
         {
             Program.account = account;
+
+            Program.story2 = new Story2.Story2();
+            Story2.Story2.LoginedEmployee = account;
+            Program.story2.FormClosed += (sender, e) => Program.mainForm.Close();
+            Program.story3 = new Story3.templateCreator();
+            Story3.templateCreator.LoginedEmployee = account;
+            Program.story3.FormClosed += (sender, e) => Program.mainForm.Close();
+            Program.story4 = new Story4.Form1();
+            Story4.Form1.LoginedEmployee = account;
+            Program.story4.FormClosed += (sender, e) => Program.mainForm.Close();
+            Program.InitializeButtonList(Program.story2);
+            Program.InitializeButtonList(Program.story3);
+            Program.InitializeButtonList(Program.story4);
+
             InitializeComponent();
         }
 
