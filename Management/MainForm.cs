f68da1b20dde18d80f8f9dc8e80c990c1c3e2a96
Gong-Hao Wei <gonghao.wei@pcc.edu>
2019-11-06 00:42:17 -0800

    fix MainForm

--- Management/MainForm.cs
+++ Management/MainForm.cs
@@ -1,47 +1,124 @@
 ﻿using AccountLibrary;
+using Story2;
+using Story3;
+using Story4;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Management
 {
-    public class MainForm : Form
+    public partial class MainForm : Form
     {
+        public Account account;
+        public NotificationSender notificationSender;
+        public TemplateCreator templateCreator;
+        public LogViewer logViewer;
+
         public MainForm(Account account)
         {
-            Program.account = account;
-
-            Program.story2 = new Story2.Story2();
-            Story2.Story2.LoginedEmployee = account;
-            Program.story2.FormClosed += (sender, e) => Program.mainForm.Close();
-            Program.story3 = new Story3.templateCreator();
-            Story3.templateCreator.LoginedEmployee = account;
-            Program.story3.FormClosed += (sender, e) => Program.mainForm.Close();
-            Program.story4 = new Story4.Form1();
-            Story4.Form1.LoginedEmployee = account;
-            Program.story4.FormClosed += (sender, e) => Program.mainForm.Close();
-            Program.InitializeButtonList(Program.story2);
-            Program.InitializeButtonList(Program.story3);
-            Program.InitializeButtonList(Program.story4);
-
             InitializeComponent();
+            this.account = account;
+
+            notificationSender = new NotificationSender();
+            NotificationSender.LoginedEmployee = account;
+            notificationSender.FormClosed += (sender, e) => Close();
+            InitializeButtonList(notificationSender);
+
+            templateCreator = new TemplateCreator();
+            TemplateCreator.LoginedEmployee = account;
+            templateCreator.FormClosed += (sender, e) => Close();
+            InitializeButtonList(templateCreator);
+
+            logViewer = new LogViewer();
+            LogViewer.LoginedEmployee = account;
+            logViewer.FormClosed += (sender, e) => Close();
+            InitializeButtonList(logViewer);
+
+            ShowInTaskbar = false;
         }
 
-        private void InitializeComponent()
+        public void InitializeButtonList(Form form)
         {
-            this.SuspendLayout();
-            // 
-            // MainForm
-            // 
-            this.ClientSize = new System.Drawing.Size(120, 0);
-            this.Name = "MainForm";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
-            this.Load += new System.EventHandler(this.MainForm_Load);
-            this.ResumeLayout(false);
+            Font font = new Font("Arial Narrow", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+
+            Button sendNotificationsButton = new Button
+            {
+                Font = font,
+                Location = new Point(10, 12),
+                Name = "sendNotificationsButton",
+                Size = new Size(210, 42),
+                Text = "Send &Notifications",
+                UseVisualStyleBackColor = true
+            };
+            sendNotificationsButton.Click += (sender, e) =>
+            {
+                if (form is NotificationSender)
+                {
+                    return;
+                }
+                notificationSender.Show();
+                notificationSender.Story2_Load(null, null);
+                form.Hide();
+            };
+            form.Controls.Add(sendNotificationsButton);
+
+            Button viewLogsButton = new Button
+            {
+                Font = font,
+                Location = new Point(235, 12),
+                Name = "viewLogsButton",
+                Size = new Size(210, 42),
+                Text = "&View Logs",
+                UseVisualStyleBackColor = true
+            };
+            viewLogsButton.Click += (sender, e) =>
+            {
+                if (form is LogViewer)
+                {
+                    return;
+                }
+                logViewer.Show();
+                form.Hide();
+            };
+            form.Controls.Add(viewLogsButton);
+
+            if (account.Role == Role.Manager)
+            {
+                Button createTemplateButton = new Button
+                {
+                    Font = font,
+                    Location = new Point(460, 12),
+                    Name = "generateTemplateButton",
+                    Size = new Size(210, 42),
+                    Text = "&Generate Template",
+                    UseVisualStyleBackColor = true
+                };
+                createTemplateButton.Click += (sender, e) =>
+                {
+                    if (form is TemplateCreator)
+                    {
+                        return;
+                    }
+                    templateCreator.Show();
+                    form.Hide();
+                };
+                form.Controls.Add(createTemplateButton);
+            }
+
+            Label nameLabel = new Label
+            {
+                Font = font,
+                Location = account.Role == Role.Manager ? new Point(685, 16) : new Point(460, 16),
+                Name = "nameLabel",
+                Size = new Size(400, 42),
+                Text = account.Role.ToString() + ", " + account.Name
+            };
+            form.Controls.Add(nameLabel);
         }
 
         private void MainForm_Load(object sender, System.EventArgs e)
         {
-            Hide();
-            Program.story2.Show();
+            notificationSender.Show();
         }
     }
 }
