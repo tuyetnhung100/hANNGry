53437c54ab8ec5fa18e933fb4eb06f07e4524227
Gong-Hao Wei <gonghao.wei@pcc.edu>
2019-11-15 01:22:28 -0800

    add Notifier project

---
+++ Notifier/NotifyCompletedEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Notifier
+{
+    public class NotifyCompletedEventArgs : EventArgs
+    {
+        public NotifyCompletedEventArgs(object userState)
+        {
+            UserState = userState;
+        }
+
+        public NotifyCompletedEventArgs(bool cancelled, Exception error, object userState)
+        {
+            Cancelled = cancelled;
+            Error = error;
+            UserState = userState;
+        }
+
+        public bool Cancelled { get; }
+        public Exception Error { get; }
+        public object UserState { get; }
+    }
+
+    public delegate void NotifyCompletedEventHandler(NotifyCompletedEventArgs e);
+}
