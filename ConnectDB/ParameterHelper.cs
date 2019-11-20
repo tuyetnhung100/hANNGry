6d751e43a4fd9a956f926df2dc01020c8198069e
Gong-Hao Wei <gonghao.wei@pcc.edu>
2019-11-20 03:55:13 -0800

    add ParameterHelper

---
+++ ConnectDB/ParameterHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConnectDB
+{
+    public static class ParameterHelper
+    {
+        public static object GetNullableValue(object obj)
+        {
+            if (obj == null)
+            {
+                return DBNull.Value;
+            }
+            return obj;
+        }
+    }
+}
