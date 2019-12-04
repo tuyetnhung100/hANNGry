a561fdfbe63d43b73079f06a8b13c5e02f56a8cd
Alex Matthias <alex.matthias@pcc.edu>
2019-12-03 17:36:22 -0800

    minor changes

--- Story4/Form3.Designer.cs
+++ Story4/Form3.Designer.cs
@@ -66,11 +66,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Location = new System.Drawing.Point(12, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(230, 25);
+            this.label1.Size = new System.Drawing.Size(221, 50);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Enter text to search for";
+            this.label1.Text = "Search for\r\nmessages containing:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form3
             // 
