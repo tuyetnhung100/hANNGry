8c1e43f8c56613402650045cc6cac66a3e12a408
Gong-Hao Wei <gonghao.wei@pcc.edu>
2019-12-09 20:31:18 -0800

    adjust story3

--- Story3/Form1.cs
+++ Story3/Form1.cs
@@ -2,7 +2,6 @@
 using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
 using TagLibrary;
 using TemplateLibrary;
@@ -27,8 +26,8 @@ namespace Story3
         // Loads the templateCreator, adds all of the tags to the tag combo box from the db.
         private void templateCreator_Load(object sender, EventArgs e)
         {
-            this.MinimumSize = new Size(this.Width, this.Height);
-            this.MaximumSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            //this.MinimumSize = new Size(this.Width, this.Height);
+            //this.MaximumSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
 
             //this.AutoSize = true;
             //this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
@@ -45,38 +44,45 @@ namespace Story3
             }
 
             reloadTemplateList();
-
-            if (!TemplateDB.Load(ref templates))
-            {
-                MessageBox.Show("Unable to retrieve templates from the database. Now closing program.", "Error!");
-                this.Close();
-            }
         }
 
         // Updates the currently selected template with whatever is in the RTB.
-        private void updateTemplate()
+        private void updateTemplate(string subject)
         {
-            Template myTemplate = new Template();
-            myTemplate.Subject = templateSelectorComboBox.SelectedItem.ToString();
-            myTemplate.Message = templateRichTextBox.Text;
-            myTemplate.CreatedAccountId = LoginedEmployee.AccountId;
-            myTemplate.CreatedDate = DateTime.Now;
-            TemplateDB.Update(myTemplate);
-            reloadTemplateList();
+            if (!IsNullOrBlank(templateSelectorComboBox))
+            {
+                Template myTemplate = new Template
+                {
+                    Subject = subject,
+                    Message = templateRichTextBox.Text,
+                    CreatedAccountId = LoginedEmployee.AccountId,
+                    CreatedDate = DateTime.Now
+                };
+                TemplateDB.Update(myTemplate);
+                reloadTemplateList();
+            }
         }
 
         // Reloads the templates and sets the index to the first item.
         private void reloadTemplateList()
         {
+            templateRichTextBox.Text = "";
             templateSelectorComboBox.Items.Clear();
 
-            Template blank = new Template();
-            blank.Subject = "";
-            blank.Message = "";
+            Template blank = new Template
+            {
+                Subject = "",
+                Message = ""
+            };
 
             templateSelectorComboBox.Items.Add(blank);
 
-            TemplateDB.Load(ref templates);
+            if (!TemplateDB.Load(ref templates))
+            {
+                MessageBox.Show("Unable to retrieve templates from the database. Now closing program.", "Error!");
+                Close();
+            }
+
             foreach (Template template in templates)
             {
                 templateSelectorComboBox.Items.Add(template);
@@ -100,35 +106,36 @@ namespace Story3
         // If the tag already exists, warns user to select from the drop down.
         private void customTagButton_Click(object sender, EventArgs e)
         {
-            string input = Interaction.InputBox("Please enter the name of the tag you would like to create: ", "New Tag", "");
+            string input = Interaction.InputBox("Please enter the name of the tag you would like to create: ", "New Tag", "").ToLower();
 
             // If the custom tag box doesn't contain the tag and the input isn't blank, it will create a new tag and insert it into the DB.
-            if (!customTagComboBox.Items.Contains(input.ToLower()) && input != "")
+            if (!customTagComboBox.Items.Contains(input) && input != "")
             {
-                Tag myTag = new Tag();
-                myTag.Name = input.ToLower();
+                Tag myTag = new Tag
+                {
+                    Name = input
+                };
                 TagDB.Add(myTag);
-                customTagComboBox.Items.Add(input.ToLower());
-                templateRichTextBox.SelectedText = "{$" + input.ToLower() + "}";
+                customTagComboBox.Items.Add(input);
+                templateRichTextBox.SelectedText = "{$" + input + "}";
             }
 
             // If the combo box contains the tag, it will alert the user and insert the tag into the text box.
-            else if (customTagComboBox.Items.Contains(input.ToLower()))
+            else if (customTagComboBox.Items.Contains(input))
             {
                 MessageBox.Show("Custom Tag already exists, inserting into the RichTextBox.", "attention");
-                templateRichTextBox.SelectedText = "{$" + input.ToLower() + "}";
-            }
-            else
-            {
-                return;
+                templateRichTextBox.SelectedText = "{$" + input + "}";
             }
         }
 
         // Inserts selected tag into RTB.
         private void customTagComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            templateRichTextBox.SelectedText = "{$" + customTagComboBox.SelectedItem.ToString() + "}";
-            templateRichTextBox.Focus();
+            if (!IsNullOrBlank(customTagComboBox))
+            {
+                templateRichTextBox.SelectedText = "{$" + customTagComboBox.SelectedItem.ToString() + "}";
+                templateRichTextBox.Focus();
+            }
         }
 
         // Saves the template to the database with the subject being the name for the template.
@@ -136,72 +143,66 @@ namespace Story3
         // Then, updates the template in the database.
         private void saveAsButton_Click(object sender, EventArgs e)
         {
+            templateErrorProvider.Clear();
+
+            // If the template RTB is blank, it will not save the template.
+            if (templateRichTextBox.Text == "")
+            {
+                templateErrorProvider.SetError(templateRichTextBox, "Cannot save a blank template! Please try again.");
+                return;
+            }
+
             using (Form3 form3 = new Form3())
             {
-                if (form3.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                if (form3.ShowDialog() == DialogResult.OK)
                 {
-                    string input = form3.SelectedText;
-                    int index = templateSelectorComboBox.FindStringExact(input);
-
-                    // If the template RTB is blank, it will not save the template.
-                    if (templateRichTextBox.Text == "")
-                    {
-                        templateErrorProvider.SetError(templateRichTextBox, "Cannot save a blank template! Please try again.");
-                        return;
-                    }
+                    string subject = form3.SelectedText;
+                    int index = templateSelectorComboBox.FindStringExact(subject);
 
                     // if index is anything but -1 (that means it's found a template with that name), then it selects that template and saves over the current text.
-                    else if (index != -1)
+                    if (index != -1)
                     {
-                        templateRichTextBox.SelectAll();
-                        templateRichTextBox.Cut();
-                        templateSelectorComboBox.SelectedIndex = index;
-                        templateRichTextBox.SelectAll();
-                        templateRichTextBox.Paste();
-                        Clipboard.Clear();
-                        updateTemplate();
-                        return;
-                    }
-
-                    // If the previous statements return false, then it saves the template as a new template.
-                    else if (input != "" || input != null)
-                    {
-                        Template myTemplate = new Template();
-                        myTemplate.Subject = input;
-                        myTemplate.Message = templateRichTextBox.Text;
-                        myTemplate.CreatedAccountId = LoginedEmployee.AccountId;
-                        myTemplate.CreatedDate = DateTime.Now;
-                        TemplateDB.Add(myTemplate);
+                        updateTemplate(subject);
                         reloadTemplateList();
                     }
 
-                    else
+                    // If the previous statements return false, then it saves the template as a new template.
+                    else if (subject != "" || subject != null)
                     {
-                        return;
-                    }
-                    templateErrorProvider.Clear();
+                        Template myTemplate = new Template
+                        {
+                            Subject = subject,
+                            Message = templateRichTextBox.Text,
+                            CreatedAccountId = LoginedEmployee.AccountId,
+                            CreatedDate = DateTime.Now
+                        };
+                        TemplateDB.Add(myTemplate);
+                        reloadTemplateList();
+                    }                 
                 }
             }
         }
 
-        // Loads a template from the DB when selected, and inserts the message column into the RTB.
+        // Switches the message column into the RTB.
         private void templateSelectorComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Template template = (Template)templateSelectorComboBox.SelectedItem;
-            templateRichTextBox.Text = template.Message;
+            if (!IsNullOrBlank(templateSelectorComboBox))
+            {
+                Template template = (Template)templateSelectorComboBox.SelectedItem;
+                templateRichTextBox.Text = template.Message;
+            }
         }
 
         // Determines whether or not the user wants to delete the selected template, and does so if they click yes.
         private void deleteButton_Click(object sender, EventArgs e)
         {
-            String currentItem = templateSelectorComboBox.SelectedItem.ToString();
-            Template template = (Template)templateSelectorComboBox.SelectedItem;
-
-            // If the subject of the template is equal to the currently selected item, then it will delete the template from the DB if the user clicks yes.
-            if (template.Subject == currentItem)
+            if (!IsNullOrBlank(templateSelectorComboBox))
             {
+                Template template = (Template)templateSelectorComboBox.SelectedItem;
+
+                // it will delete the template from the DB if the user clicks yes.
                 DialogResult delete = MessageBox.Show(
-                    "The selected template is about to be deleted!\n" +
+                    "The template \"" + template.Subject + "\"is about to be deleted!\n" +
                     "Are you sure you want to delete the template?\nTHE TEMPLATE CANNOT BE RECOVERED.",
                     "Warning!",
                     MessageBoxButtons.YesNo,
@@ -213,11 +214,6 @@ namespace Story3
                     TemplateDB.Delete(template);
                     reloadTemplateList();
                 }
-                else
-                {
-                    return;
-                }
-                reloadTemplateList();
             }
         }
 
@@ -225,8 +221,10 @@ namespace Story3
         // then it clicks the save as button and runs through that process.
         private void saveButton_Click(object sender, EventArgs e)
         {
+            templateErrorProvider.Clear();
+
             // If the text for the box either doesn't exist, or is blank, it will click the saveAs button.
-            if (templateSelectorComboBox.SelectedItem == null || templateSelectorComboBox.SelectedItem.ToString() == "")
+            if (IsNullOrBlank(templateSelectorComboBox))
             {
                 saveAsButton.PerformClick();
             }
@@ -235,15 +233,19 @@ namespace Story3
             else if (templateRichTextBox.Text == "")
             {
                 templateErrorProvider.SetError(templateRichTextBox, "Cannot save a blank template! Please try again.");
-                return;
             }
 
             // If the two previous statements are false, it will update the selected template.
             else
             {
-                updateTemplate();
+                updateTemplate(templateSelectorComboBox.SelectedItem.ToString());
                 reloadTemplateList();
             }
+        }
+
+        private bool IsNullOrBlank(ComboBox comboBox)
+        {
+            return comboBox.SelectedItem == null || comboBox.SelectedItem.ToString() == "";
         }
     }
 }
